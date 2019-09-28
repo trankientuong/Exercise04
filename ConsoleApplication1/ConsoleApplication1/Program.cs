91f7da7f7a27aed14990f2cbe72434b820cdcdaf
TranKienTuong <tuongtran1411@gmail.com>
2019-09-28 13:52:32 +0700

    Exercise04

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -6,12 +6,12 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication1
 {
-   public class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Github");
-
+            Console.WriteLine(isPrime(6));
 
         }
         public static bool isOdd(int n)
@@ -20,12 +20,48 @@ namespace ConsoleApplication1
                 return true;
             return false;
         }
-       public static bool isEven(int n)
+        public static bool isEven(int n)
         {
             if (n % 2 == 0)
                 return true;
             return false;
         }
-       
+        public static bool isPrime(int n)
+        {
+            int count = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                    count++;
+            }
+            if (count == 2)
+                return true;
+            return false;
+        }
+        public static int Square(int n)
+        {
+            return n * n;
+        }
+        public static int Cube(int n)
+        {
+            return n * n * n;
+        }
+        public static int Pow(int x, int y)
+        {
+            int R = 1;
+            for(int i=1;i<=y;i++)
+            {
+                R = R * x;
+            }
+            return R;
+         }
+        public static int Abs(int n)
+        {
+            if (n > 0)
+                n = n;
+            else
+                n = n * (-1);
+            return n;
+        }
     }
 }
--- ConsoleApplication1/UnitTestProject1/UnitTest1.cs
+++ ConsoleApplication1/UnitTestProject1/UnitTest1.cs
@@ -23,6 +23,37 @@ namespace UnitTestProject1
             bool result4 = Program.isEven(4);
             Assert.AreEqual(true, result4);
         }
-       
+        [TestMethod]
+        public void TestIsPrime()
+        {
+            bool result7 = Program.isPrime(7);
+            Assert.AreEqual(true, result7);
+            bool result6 = Program.isPrime(6);
+            Assert.AreEqual(false, result6);
+        }
+        [TestMethod]
+        public void TestSquare()
+        {
+            int result4 = Program.Square(4);
+            Assert.AreEqual(16, result4);
+        }
+        [TestMethod]
+        public void TestCube()
+        {
+            int result4 = Program.Cube(4);
+            Assert.AreEqual(64, result4);
+        }
+        [TestMethod]
+        public void TestPow()
+        {
+            int result4 = Program.Pow(2,5);
+            Assert.AreEqual(32, result4);
+        }
+        [TestMethod]
+        public void TestAbs()
+        {
+            int result = Program.Abs(4);
+            Assert.AreEqual(4, result);
+        }
     }
 }
